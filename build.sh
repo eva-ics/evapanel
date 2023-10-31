9ab99ae43249e8093a52c9e6868d80ea5262706d
Serhij S <div@altertech.com>
2023-11-01 00:31:55 +0100

    build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 
 ARCH=$(uname -m)
 ARCH=$(echo "${ARCH}" | sed 's/arm.*/arm/g')
-ARCH=$ARCH cargo build || exit 1
+ARCH=$ARCH cargo build --release || exit 1
 strip target/release/evapanel
 echo $ARCH
